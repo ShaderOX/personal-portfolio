6f915b8d2e8ad662710ff1c1b0cd69daad28075b
Asadullah Kashif <s.asad18k@gmail.com>
2022-08-30 01:04:07 +0500

    added the DataContext Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import DataContext from "./context/DataContext";
+import data from "./data.json";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <DataContext.Provider value={data}>
+      <App />
+    </DataContext.Provider>
   </React.StrictMode>
 );
